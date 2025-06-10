e8544cc835a93bbb65d933ea19b02bceafb85597
Ekaterina Karelova <kulibinka1987@gmail.com>
2025-06-10 09:21:32 +0300

    added data.py

---
+++ pinterest_project/data/data.py
@@ -0,0 +1,9 @@
+
+test_email = 'e.karelova2303@gmail.com'
+test_pass = 'Kk123456'
+description = 'Здесь моя история'
+web_site = 'http://mysite.ru'
+profile_name = 'E Karelova'
+profile_username = 'ekarelova2303'
+follower_count = '0 подписок'
+text_search = 'Лето'
