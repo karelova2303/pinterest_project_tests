5cd0541f2b01b327965341ddd466dd8b6bfb069f
Ekaterina Karelova <kulibinka1987@gmail.com>
2025-06-13 21:08:08 +0300

    added launch via Jenkins

--- tests/ui/conftest.py
+++ tests/ui/conftest.py
@@ -1,13 +1,56 @@
+import os
+
 import allure_commons
 import pytest
+from dotenv import load_dotenv
 from selene import browser, support
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 from pinterest_project.models.app import app
 from pinterest_project.utils import attach
 
+DEFAULT_BROWSER_VERSION = '128.0'
+
+
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='128.0'
+    )
+
 
 @pytest.fixture(scope='session')
-def browser_manager():
+def browser_manager(request):
+    selenoid_login = os.getenv("SELENOID_LOGIN")
+    selenoid_pass = os.getenv("SELENOID_PASS")
+    selenoid_url = os.getenv("SELENOID_URL")
+
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != '' else DEFAULT_BROWSER_VERSION
+    options = Options()
+
+    selenoid_capabilities = {
+        "browserName": 'chrome',
+        "browserVersion": browser_version,
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True,
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+
+    driver = webdriver.Remote(
+        command_executor=f'https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub',
+        options=options
+    )
+
+    browser.config.driver = driver
     browser.config.base_url = 'https://ru.pinterest.com'
     browser.driver.maximize_window()
     browser.config.timeout = 15.0
@@ -16,6 +59,7 @@ def browser_manager():
 
     browser.quit()
 
+
 @pytest.fixture(scope='class')
 def authorization_user(browser_manager):
     app.authorization.login_of_account()
@@ -23,6 +67,7 @@ def authorization_user(browser_manager):
     yield
 
     app.authorization.log_out_of_account()
+
 
 @pytest.fixture(scope='function')
 def attach_with_test():
